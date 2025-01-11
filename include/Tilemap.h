c5be81292ce6a116d4be31879353e9c95a2aca89
Kero <kerowagdy2018@gmail.com>
2025-01-11 21:42:22 +0200

    Tilemap class ready

---
+++ include/Tilemap.h
@@ -0,0 +1,72 @@
+// Tilemap.h
+///////////////////////////////////////////
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+    bool load(  const std::filesystem::path& tileset, /* texture/tileset filepath */
+                sf::Vector2u    tileSize, /* tileSize in the texture*/
+                const uint16_t* tiles,    /* indexes of tiles in the texture */
+                uint16_t        width,    /* map width */
+                uint16_t        height    /* map height */)
+    {
+        // Open texture file
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+        
+        // setup vertex array
+        m_vertices.setPrimitiveType(sf::PrimitiveType::Triangles);
+        m_vertices.resize(width * height * 6);
+
+        for (uint16_t i = 0; i < height; ++i)
+        {
+            for (uint16_t j = 0; j < width; ++j)
+            {
+
+                uint16_t tileNumber = tiles[j + i * width];
+
+                // tile's pos on tileset
+                uint16_t tu = tileNumber * tileSize.x;
+                uint16_t tv = 0;
+
+                // vertices of current tile
+                sf::Vertex* triangles = &m_vertices[(j + i * width) * 6];
+
+                // define current tile's vertices position
+                triangles[0].position = sf::Vector2f{j * tileSize.x, i * tileSize.y}; // top-left
+                triangles[1].position = sf::Vector2f{(j+1) * tileSize.x, i * tileSize.y}; // top-right
+                triangles[2].position = sf::Vector2f{j * tileSize.x, (i+1) * tileSize.y}; // bottom-left
+                triangles[3].position = sf::Vector2f{j * tileSize.x, (i+1) * tileSize.y}; // bottom-left
+                triangles[4].position = sf::Vector2f{(j+1) * tileSize.x, (i+1) * tileSize.y}; // bottom-right
+                triangles[5].position = sf::Vector2f{(j+1) * tileSize.x, i * tileSize.y}; // top-right
+
+
+                // define the matching texture coordinates
+                triangles[0].texCoords = sf::Vector2f{tu, tv}; // top-left
+                triangles[1].texCoords = sf::Vector2f{tu + tileSize.x, tv}; // top-right
+                triangles[2].texCoords = sf::Vector2f{tu, tv + tileSize.y}; // bottom-left
+                triangles[3].texCoords = sf::Vector2f{tu, tv + tileSize.y}; // bottom-left
+                triangles[4].texCoords = sf::Vector2f{tu + tileSize.x, tv + tileSize.y}; // bottom-right
+                triangles[5].texCoords = sf::Vector2f{tu + tileSize.x, tv}; // top-right
+            }
+        }
+        return true;
+    }
+private:
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+    {
+        // apply the transform
+        states.transform = getTransform();
+        // apply the texture
+        states.texture = &m_tileset;
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+
+    sf::VertexArray m_vertices;
+    sf::Texture     m_tileset;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,41 @@
 #include <SFML/Graphics.hpp>
+#include <Tilemap.h>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
-    window.setFramerateLimit(60);
+    sf::RenderWindow window(sf::VideoMode({16*32, 8*32}), "Testing Tilemap");
+    // define the level with an array of tile indices
+    constexpr std::array<uint16_t, 16 * 8> level = {
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
 
+    TileMap map;
+    if (!map.load("res/sfml-test-tileset.png", {32, 32}, level.data(), 16, 8))
+        return -1;
+
+    // run the main loop
     while (window.isOpen())
     {
+        // handle events
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
+        // draw the map
         window.clear();
+        window.draw(map);
         window.display();
     }
+
+    
+
 }
