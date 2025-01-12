69483559fabfa759bbe7d53a1b95df638c075d30
Kero <kerowagdy2018@gmail.com>
2025-01-12 13:14:24 +0200

    Clean main & Encapsulate Game

--- include/Game.h
+++ include/Game.h
@@ -37,34 +37,110 @@ namespace game
     };
 
     
-    void generateLevel(game::Tile tiles[], uint16_t width, uint16_t height)
-    {
-        srand(time(0));
-        uint16_t size = width * height;
-        uint16_t nMines = size / 3;
-        for (uint16_t mine = 0; mine < nMines; ++mine)
-        {
-            uint16_t mineIndex;
-            do
-            {
-                mineIndex = rand() % size;
-            } while (tiles[mineIndex].m_isMine);
-            tiles[mineIndex].m_isMine = true;
-            uint16_t i = mineIndex / height;
-            uint16_t j = mineIndex % height;
-            if (i != height - 1)
-                tiles[mineIndex + width].m_mineCounter++;
-            if (j != width - 1)
-                tiles[mineIndex + 1].m_mineCounter++;
-            if (i != height - 1 && j != width - 1)
-                tiles[mineIndex + width + 1].m_mineCounter++;
-            if (i != 0)
-                tiles[mineIndex - width].m_mineCounter++;
-            if (j != 0)
-                tiles[mineIndex - 1].m_mineCounter++;
-            if (i != 0 && j != 0)
-                tiles[mineIndex - width - 1].m_mineCounter++;
-        }
-    }
 
+    class Game
+    {
+    public:
+        Game()
+        {
+        }
+
+        bool init(const std::filesystem::path& tilesetPath)
+        {
+            // Setup window
+            window.setFramerateLimit(60);
+            // Generate new level
+            generateLevel();
+            // map level to tileset indices
+            updateMapIndices();
+            // Generate Tilemap
+            return tilemap.load(tilesetPath, {tileSize, tileSize}, mapIndices, width, height);
+        }
+
+        // Game/Main Loop
+        void run()
+        {
+            while (window.isOpen())
+            {
+                // handle events
+                while (const std::optional event = window.pollEvent())
+                    handleEvent(event);
+
+                // window drawing
+                window.clear();
+                window.draw(tilemap);
+                window.display();
+            }
+            
+        }
+
+    private:
+        void generateLevel()
+        {
+            srand(time(0));
+            uint16_t size = width * height;
+
+            for (uint16_t mine = 0; mine < mines; ++mine)
+            {
+                uint16_t mineIndex;
+                do
+                {
+                    mineIndex = rand() % size;
+                } while (tiles[mineIndex].m_isMine);
+                tiles[mineIndex].m_isMine = true;
+
+                // Update neighbours' counter
+                uint16_t i = mineIndex / height;
+                uint16_t j = mineIndex % height;
+                if (i != height - 1)
+                {
+                    tiles[mineIndex + width].m_mineCounter++; // down
+                    if (j != width - 1)
+                        tiles[mineIndex + width + 1].m_mineCounter++; // down-right
+                    if (j != 0)
+                        tiles[mineIndex + width - 1].m_mineCounter++; // down-left
+                }
+
+                if (i != 0)
+                {
+                    tiles[mineIndex - width].m_mineCounter++; // up
+                    if (j != 0)
+                        tiles[mineIndex - width - 1].m_mineCounter++; // up-left
+                    if (j != width - 1)
+                        tiles[mineIndex - width + 1].m_mineCounter++; // up-right
+                }
+
+                if (j != width - 1)
+                    tiles[mineIndex + 1].m_mineCounter++; // right
+                if (j != 0)
+                    tiles[mineIndex - 1].m_mineCounter++; // left
+            }
+        }
+
+        void updateMapIndices()
+        {
+            for (uint16_t i = 0; i < width * height; i++)
+                mapIndices[i] = tiles[i].getMapIndex();
+        }
+    
+        void handleEvent(const std::optional<sf::Event>& event)
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    private:
+        // Static Data
+        static const uint16_t width = 9u; // 2-bytes
+        static const uint16_t height = 9u; // 2-bytes
+        static const uint16_t tileSize = 64u; // 2-bytes
+        ////////////////////////////////////////////////
+
+        // Member Fields
+        uint16_t mines = (width * height) / 3;
+        sf::RenderWindow window { sf::VideoMode({width * tileSize, height * tileSize}), "Minesweeper" }; // unknown-bytes
+        TileMap tilemap;
+        Tile tiles[width * height]; // 9 * 9 * 4 = 324-bytes
+        uint16_t mapIndices[width * height];
+        ////////////////////////////////////////////////
+    };
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,9 @@
 #include <Tilemap.h>
 #include <Game.h>
 
-template <short size>
-void tilesToMapIndex(game::Tile level[], uint16_t indices[])
-{
-    for (uint16_t i = 0; i < size; i++)
-        indices[i] = level[i].getMapIndex();
-}
-
-const uint16_t WIDTH = 9;
-const uint16_t HEIGHT = 9;
-const uint16_t TILE_SIZE = 64;
-
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({WIDTH*TILE_SIZE, HEIGHT*TILE_SIZE}), "Testing My Tilemap");
-    game::Tile level[WIDTH*HEIGHT];
-    uint16_t mapIndices[WIDTH*HEIGHT];
-    game::generateLevel(level, WIDTH, HEIGHT);
-    tilesToMapIndex<WIDTH*HEIGHT>(level, mapIndices);
-    
-
-    TileMap map;
-    if (!map.load("res/png/tilemap-new.png", {TILE_SIZE, TILE_SIZE}, mapIndices, WIDTH, HEIGHT))
-        return -1;
-
-    // run the main loop
-    while (window.isOpen())
-    {
-        // handle events
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        // draw the map
-        window.clear();
-        window.draw(map);
-        window.display();
-    }
+    game::Game game;
+    game.init("res/png/tilemap-new.png");
+    game.run();
 }
