e6a77bd86c32761c9397ffb24921b4ce270ef9a6
Kero <kerowagdy2018@gmail.com>
2025-01-12 23:50:26 +0200

    Handling Events in Progress

--- include/Game.h
+++ include/Game.h
@@ -6,6 +6,17 @@
 
 namespace game
 {
+    // Convert Dimensions (1D to 2D)
+    sf::Vector2u convertD12(uint16_t oneDim, uint16_t width)
+    {
+        return {oneDim / width, oneDim % width};
+    }
+    // Convert Dimensions (2D to 1D)
+    template <class T>
+    T convertD21(const sf::Vector2<T>& twoDim, uint16_t width)
+    {
+        return twoDim.y + twoDim.x * width;
+    }
     enum mapIndex {
         empty = 0, one, two, three, four, five, six, seven, eight,
         hidden, flag, mine, mineClicked
@@ -17,22 +28,37 @@ namespace game
     public:
         enum class state : char // 1-byte
         {
-            hidden, notHidden, flagged
+            hidden, notHidden, flagged, peek, mineClicked
         };
         short m_mineCounter = 0; // 2-bytes
         bool  m_isMine = 0; // 1-byte
-        state m_state = state::notHidden; // 1-byte // TODO: Let Init State = Hidden
+        state m_state = state::hidden; // 1-byte // TODO: Let Init State = Hidden
 
-        mapIndex getMapIndex()
+        mapIndex getMapIndex() const {
+            return getMapIndex(*this);
+        }
+        static mapIndex getMapIndex(const Tile& tile)
         {
-            if (m_state == state::hidden)
+            const auto& st = tile.m_state;
+            const auto& isMine = tile.m_isMine;
+            const auto& mineCounter = tile.m_mineCounter;
+            if (st == state::hidden)
                 return mapIndex::hidden;
-            else if (m_state == state::flagged)
+
+            else if (st == state::flagged)
                 return mapIndex::flag;
-            else if (m_isMine) // TODO: handle mineClicked case
+
+            else if (st == state::peek)
+                return mapIndex::empty;
+
+            else if (st == state::mineClicked && isMine)
+                return mapIndex::mineClicked;
+
+            else if (isMine)
                 return mapIndex::mine;
+
             else
-                return static_cast<mapIndex>(m_mineCounter);
+                return static_cast<mapIndex>(mineCounter);
         }
     };
 
@@ -90,30 +116,11 @@ namespace game
                 tiles[mineIndex].m_isMine = true;
 
                 // Update neighbours' counter
-                uint16_t i = mineIndex / height;
-                uint16_t j = mineIndex % height;
-                if (i != height - 1)
-                {
-                    tiles[mineIndex + width].m_mineCounter++; // down
-                    if (j != width - 1)
-                        tiles[mineIndex + width + 1].m_mineCounter++; // down-right
-                    if (j != 0)
-                        tiles[mineIndex + width - 1].m_mineCounter++; // down-left
-                }
+                Tile* neighbours[8];
+                auto counter = getNeighbours(mineIndex, neighbours);
+                for (uint16_t i = 0; i < counter; i++)
+                    neighbours[i]->m_mineCounter++;
 
-                if (i != 0)
-                {
-                    tiles[mineIndex - width].m_mineCounter++; // up
-                    if (j != 0)
-                        tiles[mineIndex - width - 1].m_mineCounter++; // up-left
-                    if (j != width - 1)
-                        tiles[mineIndex - width + 1].m_mineCounter++; // up-right
-                }
-
-                if (j != width - 1)
-                    tiles[mineIndex + 1].m_mineCounter++; // right
-                if (j != 0)
-                    tiles[mineIndex - 1].m_mineCounter++; // left
             }
         }
 
@@ -123,11 +130,221 @@ namespace game
                 mapIndices[i] = tiles[i].getMapIndex();
         }
     
+        void updateTile(uint16_t tileIndex1D, const Tile::state& state)
+        {
+            tiles[tileIndex1D].m_state = state;
+            auto tileIndex2D = convertD12(tileIndex1D, width);
+            tilemap.updateTile(tileIndex2D.x, tileIndex2D.y, tiles[tileIndex1D].getMapIndex());
+        }
+
+        void updateTile(Tile* tilePtr, const Tile::state& state)
+        {
+            if (tilePtr == nullptr)
+                return;
+            tilePtr->m_state = state;
+            uint16_t tileIndex1D = tilePtr - tiles;
+            auto tileIndex2D = convertD12(tileIndex1D, width);
+            tilemap.updateTile(tileIndex2D.x, tileIndex2D.y, tiles[tileIndex1D].getMapIndex());
+        }
+
+        // Fills given array of pointers and returns its size
+        uint16_t getNeighbours(const uint16_t tileIndex1D, Tile** neighbours)
+        {
+            sf::Vector2u tileIndex2D = convertD12(tileIndex1D, width);
+            auto& i = tileIndex2D.x;
+            auto& j = tileIndex2D.y;
+            uint16_t counter = 0;
+            if (i != height - 1)
+            {
+                neighbours[counter] = &tiles[tileIndex1D + width];
+                counter++;
+                if (j != width - 1)
+                {
+                    neighbours[counter] = &tiles[tileIndex1D + width + 1]; // down-right
+                    counter++;
+                }
+                if (j != 0)
+                {
+                    neighbours[counter] = &tiles[tileIndex1D + width - 1]; // down-left
+                    counter++;
+                }
+            }
+
+            if (i != 0)
+            {
+                neighbours[counter] = &tiles[tileIndex1D - width]; // up
+                counter++;
+                if (j != 0)
+                {
+                    neighbours[counter] = &tiles[tileIndex1D - width - 1]; // up-left
+                    counter++;
+                }
+                if (j != width - 1)
+                {
+                    neighbours[counter] = &tiles[tileIndex1D - width + 1]; // up-right
+                    counter++;
+                }
+            }
+
+            if (j != width - 1)
+            {
+                neighbours[counter] = &tiles[tileIndex1D + 1]; // right
+                counter++;
+            }
+            if (j != 0)
+            {
+                neighbours[counter] = &tiles[tileIndex1D - 1]; // left
+                counter++;
+            }
+            return counter;
+        }
+
         void handleEvent(const std::optional<sf::Event>& event)
         {
+            static bool wasPeeking = false;
+            static uint16_t tilePeekedIndex1D = 0;
+            // last hovered tile
+            static uint16_t hoveredIndex = 0;
+
             if (event->is<sf::Event::Closed>())
                 window.close();
+
+            else if (const auto* mouse = event->getIf<sf::Event::MouseMoved>())
+            {
+
+                // PEEK ON HOVER
+                uint16_t col, row;
+                {
+                    auto tileIndex2D = tileIndexFromScreenPos(mouse->position);
+                    col = tileIndex2D.y;
+                    row = tileIndex2D.x;
+                }
+                // hover on different tile
+                if (hoveredIndex != col + row * width)
+                {
+                    // hide prev hovered tile 
+                    auto hoveredIndex2D = convertD12(hoveredIndex, width);
+                    if (tiles[hoveredIndex].m_state == Tile::state::hidden)
+                        tilemap.updateTile(hoveredIndex2D.x, hoveredIndex2D.y, mapIndex::hidden);
+                    // update current hovered tile
+                    hoveredIndex = col + row * width;
+                    Tile temp = tiles[hoveredIndex];
+                    temp.m_state = Tile::state::notHidden;
+                    tilemap.updateTile(row, col, temp.getMapIndex());
+                }
+            }
+
+            else if (const auto* mouse = event->getIf<sf::Event::MouseButtonReleased>())
+            {
+                if (wasPeeking)
+                {
+                    Tile* neighborus[8];
+                    auto counter = getNeighbours(tilePeekedIndex1D, neighborus);
+                    for (uint16_t i = 0; i < counter; i++)
+                    {
+                        if (neighborus[i]->m_state == Tile::state::peek)
+                            updateTile(neighborus[i], Tile::state::hidden);
+                    }
+                    wasPeeking = false;
+                }
+            }
+            else if (const auto* mouse = event->getIf<sf::Event::MouseButtonPressed>())
+            {
+                bool left = sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
+                bool right = sf::Mouse::isButtonPressed(sf::Mouse::Button::Right);
+                // Both Buttons Clicked (Peeking Neighbours)
+                auto tileIndex1D = convertD21(tileIndexFromScreenPos(mouse->position), width);
+                auto tileIndex2D = tileIndexFromScreenPos(mouse->position);
+                if (left && right)
+                {
+                    Tile* neighbours[8];
+                    auto counter = getNeighbours(tileIndex1D, neighbours);
+
+                    for (uint16_t i = 0; i < counter; i++)
+                    {
+                        if (neighbours[i]->m_state == Tile::state::hidden)
+                            updateTile(neighbours[i], Tile::state::peek);
+                    }
+                    wasPeeking = true;
+                    tilePeekedIndex1D = tileIndex1D;
+                }
+                else if (left)
+                {
+                    if (tiles[tileIndex1D].m_isMine)
+                    {
+                        updateTile(tileIndex1D, Tile::state::mineClicked);
+                        endGame();
+                    }
+                    else
+                    {
+                        updateTile(tileIndex1D, Tile::state::notHidden);
+                        if (tiles[tileIndex1D].m_mineCounter == 0)
+                            unhideEmptyNeighbours(tileIndex1D);
+
+                    }
+                }
+                else if (right)
+                {
+                    if (tiles[tileIndex1D].m_state == Tile::state::hidden)
+                        updateTile(tileIndex1D, Tile::state::flagged);
+                }
+                
+
+            }
         }
+
+        void unhideEmptyNeighbours(uint16_t index1D)
+        {
+            auto index2D = convertD12(index1D, width);
+            auto& row = index2D.x;
+            auto& col = index2D.y;
+            // Row of Clicked Empty
+            for (; col < width; ++col)
+            {
+                index1D = convertD21(index2D, width);
+                if (tiles[index1D].m_mineCounter != 0)
+                    break;
+                updateTile(index1D, Tile::state::notHidden);
+            }
+            if (col > 0)
+                col--;
+            for (; col >= 0; --col)
+            {
+                index1D = convertD21(index2D, width);
+                if (tiles[index1D].m_mineCounter != 0)
+                    break;
+                updateTile(index1D, Tile::state::notHidden);
+                if (col == 0)
+                    break;
+            }
+
+            // Upper rows
+            for (; row >= 0; row--)
+            {
+                // if ()
+                if (row == 0)
+                    break;
+            }
+
+        }
+
+        void endGame()
+        {
+            std::cout << "Game Finished\n";
+            for (uint16_t i = 0; i < width * height; ++i)
+            {
+                if (tiles[i].m_state != Tile::state::mineClicked && tiles[i].m_isMine)
+                    updateTile(i, Tile::state::notHidden);
+            }
+        }
+
+        static sf::Vector2u tileIndexFromScreenPos(const sf::Vector2i& mousePos)
+        {
+            if (mousePos.x < 0 || mousePos.y < 0)
+                return {};
+            return {mousePos.y / tileSize, mousePos.x / tileSize};
+        }
+
     private:
         // Static Data
         static const uint16_t width = 9u; // 2-bytes
@@ -136,11 +353,12 @@ namespace game
         ////////////////////////////////////////////////
 
         // Member Fields
-        uint16_t mines = (width * height) / 3;
+        // uint16_t mines = (width * height) / 5; // TODO: change it 
+        uint16_t mines = 10; // TODO: change it 
         sf::RenderWindow window { sf::VideoMode({width * tileSize, height * tileSize}), "Minesweeper" }; // unknown-bytes
         TileMap tilemap;
         Tile tiles[width * height]; // 9 * 9 * 4 = 324-bytes
-        uint16_t mapIndices[width * height];
+        uint16_t mapIndices[width * height]; // 9 * 9 * 2 = 162-bytes
         ////////////////////////////////////////////////
     };
 };
--- include/Tilemap.h
+++ include/Tilemap.h
@@ -13,6 +13,9 @@ public:
                 uint16_t        width,    /* map width */
                 uint16_t        height    /* map height */)
     {
+        // update member data
+        m_tileSize = tileSize;
+        m_width = width;
         // Open texture file
         if (!m_tileset.loadFromFile(tileset))
             return false;
@@ -27,39 +30,49 @@ public:
             {
 
                 uint16_t tileNumber = tiles[j + i * width];
-
-                // tile's pos on tileset
-                uint16_t tu = (tileNumber % (m_tileset.getSize().x / tileSize.x)) * tileSize.x;
-                uint16_t tv = (tileNumber / (m_tileset.getSize().x / tileSize.x)) * tileSize.y;
-                // uint16_t tu = tileNumber * tileSize.x;
-                // uint16_t tv = 0;
-                // the commented code work well if tileNumber is 100% in range of [0, tileSize - 1]
-                // but the uncommented code makes it work even if any tileNubmber exceed the range
-
-
-                // vertices of current tile
-                sf::Vertex* triangles = &m_vertices[(j + i * width) * 6];
-
-                // define current tile's vertices position
-                triangles[0].position = sf::Vector2f{j * tileSize.x, i * tileSize.y}; // top-left
-                triangles[1].position = sf::Vector2f{(j+1) * tileSize.x, i * tileSize.y}; // top-right
-                triangles[2].position = sf::Vector2f{j * tileSize.x, (i+1) * tileSize.y}; // bottom-left
-                triangles[3].position = sf::Vector2f{j * tileSize.x, (i+1) * tileSize.y}; // bottom-left
-                triangles[4].position = sf::Vector2f{(j+1) * tileSize.x, (i+1) * tileSize.y}; // bottom-right
-                triangles[5].position = sf::Vector2f{(j+1) * tileSize.x, i * tileSize.y}; // top-right
-
-
-                // define the matching texture coordinates
-                triangles[0].texCoords = sf::Vector2f{tu, tv}; // top-left
-                triangles[1].texCoords = sf::Vector2f{tu + tileSize.x, tv}; // top-right
-                triangles[2].texCoords = sf::Vector2f{tu, tv + tileSize.y}; // bottom-left
-                triangles[3].texCoords = sf::Vector2f{tu, tv + tileSize.y}; // bottom-left
-                triangles[4].texCoords = sf::Vector2f{tu + tileSize.x, tv + tileSize.y}; // bottom-right
-                triangles[5].texCoords = sf::Vector2f{tu + tileSize.x, tv}; // top-right
+                updateTile(i, j, tileNumber);
             }
         }
         return true;
     }
+
+    /**
+     * @brief 
+     * 
+     * @param i row
+     * @param j col
+     * @param tileNumber newTileIndex on the tileset
+     */
+    void updateTile(uint16_t i, uint16_t j, uint16_t tileNumber)
+    {
+        // tile's pos on tileset
+        uint16_t tu = (tileNumber % (m_tileset.getSize().x / m_tileSize.x)) * m_tileSize.x;
+        uint16_t tv = (tileNumber / (m_tileset.getSize().x / m_tileSize.x)) * m_tileSize.y;
+        // uint16_t tu = tileNumber * m_tileSize.x;
+        // uint16_t tv = 0;
+        // the commented code work well if tileNumber is 100% in range of [0, tileSize - 1]
+        // but the uncommented code makes it work even if any tileNubmber exceed the range
+
+        // vertices of current tile
+        sf::Vertex* triangles = &m_vertices[(j + i * m_width) * 6];
+
+        // define current tile's vertices position
+        triangles[0].position = sf::Vector2f{j * m_tileSize.x, i * m_tileSize.y}; // top-left
+        triangles[1].position = sf::Vector2f{(j+1) * m_tileSize.x, i * m_tileSize.y}; // top-right
+        triangles[2].position = sf::Vector2f{j * m_tileSize.x, (i+1) * m_tileSize.y}; // bottom-left
+        triangles[3].position = sf::Vector2f{j * m_tileSize.x, (i+1) * m_tileSize.y}; // bottom-left
+        triangles[4].position = sf::Vector2f{(j+1) * m_tileSize.x, (i+1) * m_tileSize.y}; // bottom-right
+        triangles[5].position = sf::Vector2f{(j+1) * m_tileSize.x, i * m_tileSize.y}; // top-right
+
+        // define the matching texture coordinates
+        triangles[0].texCoords = sf::Vector2f{tu, tv}; // top-left
+        triangles[1].texCoords = sf::Vector2f{tu + m_tileSize.x, tv}; // top-right
+        triangles[2].texCoords = sf::Vector2f{tu, tv + m_tileSize.y}; // bottom-left
+        triangles[3].texCoords = sf::Vector2f{tu, tv + m_tileSize.y}; // bottom-left
+        triangles[4].texCoords = sf::Vector2f{tu + m_tileSize.x, tv + m_tileSize.y}; // bottom-right
+        triangles[5].texCoords = sf::Vector2f{tu + m_tileSize.x, tv}; // top-right
+    }
+
 private:
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override
     {
@@ -74,4 +87,6 @@ private:
 
     sf::VertexArray m_vertices;
     sf::Texture     m_tileset;
+    sf::Vector2u    m_tileSize;
+    uint16_t        m_width;
 };
